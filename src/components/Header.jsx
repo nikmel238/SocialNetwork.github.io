03b0e1095a00e9739acb0d2adaf73c92d68f533c
melnik.ita <man.nikita0@yandex.ru>
2023-10-15 20:55:29 +0300

    Creating a react-project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+        <img src="https://free-png.ru/wp-content/uploads/2021/07/free-png.ru-53.png"></img>
+      </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+  return (
+    <div className="content">
+        <div>
+          <img src="https://s1.1zoom.me/b5050/813/421743-svetik_3840x2400.jpg"></img>
+        </div>
+        <div>
+          <img src="https://pixelbox.ru/wp-content/uploads/2022/03/lol-avatars-pixelbox.ru-52.jpg"></img>
+        </div>
+        <div>
+          My post
+          <div>New Post</div>
+        </div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+  )
+}
+
+export default Profile;
