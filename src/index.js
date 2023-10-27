2c1e203fd5da43e6f2bce648762da50771f8fdac
NikMel238 <man.nikita0@yandex.ru>
2023-10-27 11:27:34 +0300

    lesson 37

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {renderEntireTree} from "./render";
-import state from "./redux/state";
+import store from "./redux/state";
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App";
 
 
-
-renderEntireTree(state);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let renderEntireTree = () => {
+    root.render(
+        <React.StrictMode>
+            <App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
+        </React.StrictMode>
+    );
+}
+renderEntireTree();
+store.subscribe(renderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,101 +1,108 @@
-import {renderEntireTree} from "../render";
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {
+                    id: 1,
+                    message: "Hi, how are you?",
+                    likeCount: 15,
+                },
+                {
+                    id: 2,
+                    message: "It's my first post",
+                    likeCount: 25,
+                },
+                {
+                    id: 3,
+                    message: "RUSSIA",
+                    likeCount: 2,
+                },
+                {
+                    id: 4,
+                    message: "STRIKE",
+                    likeCount: 22,
+                },
+            ],
+            newPostText: `it-kamasutra`,
+        },
+        messagesPage: {
+            dialogs: [
+                {
+                    id: 1,
+                    name: "Dmitriy",
+                    logo: "https://sopranoclub.ru/images/memy-na-avu-275-memnyh-avatarok/file56870.jpeg"
+                },
+                {
+                    id: 2,
+                    name: "Valeriy",
+                    logo: "https://pic.rutubelist.ru/video/46/b2/46b25de0bf3d3fa60a22037e55071354.jpg"
+                },
+                {
+                    id: 3,
+                    name: "Marat",
+                    logo: "https://pixelbox.ru/wp-content/uploads/2021/11/avatar-whatsapp-pixelbox.ru-36.jpg"
+                },
+                {
+                    id: 4,
+                    name: "Kolya",
+                    logo: "https://pixelbox.ru/wp-content/uploads/2022/08/avatars-viber-pixelbox.ru-33.jpg"
+                },
+                {
+                    id: 5,
+                    name: "Pavel",
+                    logo: "https://pixelbox.ru/wp-content/uploads/2022/08/avatar-boy-telegram-pixelbox.ru-88.jpg"
+                },
+            ],
+            messages: [
+                {
+                    id: 1,
+                    message: "Hello, world!",
+                },
+                {
+                    id: 2,
+                    message: "Hello, Russia!!!",
+                },
+                {
+                    id: 3,
+                    message: "Yo",
+                },
+                {
+                    id: 4,
+                    message: "Balalayka",
+                },
+            ],
+        },
+        navbar: {
+            menu: ["Profile", "Message", "News", "Music", "Settings"],
 
-let state = {
-    profilePage: {
-        posts: [
-            {
-                id: 1,
-                message: "Hi, how are you?",
-                likeCount: 15,
-            },
-            {
-                id: 2,
-                message: "It's my first post",
-                likeCount: 25,
-            },
-            {
-                id: 3,
-                message: "RUSSIA",
-                likeCount: 2,
-            },
-            {
-                id: 4,
-                message: "STRIKE",
-                likeCount: 22,
-            },
-        ],
-        newPostText: `it-kamasutra`,
+        }},
+    getState() {
+        return this._state;
     },
-    messagesPage: {
-        dialogs: [
-            {
-                id: 1,
-                name: "Dmitriy",
-                logo: "https://sopranoclub.ru/images/memy-na-avu-275-memnyh-avatarok/file56870.jpeg"
-            },
-            {
-                id: 2,
-                name: "Valeriy",
-                logo: "https://pic.rutubelist.ru/video/46/b2/46b25de0bf3d3fa60a22037e55071354.jpg"
-            },
-            {
-                id: 3,
-                name: "Marat",
-                logo: "https://pixelbox.ru/wp-content/uploads/2021/11/avatar-whatsapp-pixelbox.ru-36.jpg"
-            },
-            {
-                id: 4,
-                name: "Kolya",
-                logo: "https://pixelbox.ru/wp-content/uploads/2022/08/avatars-viber-pixelbox.ru-33.jpg"
-            },
-            {
-                id: 5,
-                name: "Pavel",
-                logo: "https://pixelbox.ru/wp-content/uploads/2022/08/avatar-boy-telegram-pixelbox.ru-88.jpg"
-            },
-        ],
-        messages: [
-            {
-                id: 1,
-                message: "Hello, world!",
-            },
-            {
-                id: 2,
-                message: "Hello, Russia!!!",
-            },
-            {
-                id: 3,
-                message: "Yo",
-            },
-            {
-                id: 4,
-                message: "Balalayka",
-            },
-        ],
-    },
-    navbar: {
-        menu: ["Profile", "Message", "News", "Music", "Settings"],
+    _callSubscriber() {},
+    addPost () {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likeCount: 0,
+        };
+        this._state.profilePage.newPostText = ``;
+        this._state.profilePage.posts.push(newPost);
 
+        this._callSubscriber();
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber();
+    },
+    subscribe (observer)  {
+        this._callSubscriber = observer;
     }
 }
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 0,
-    };
-    state.profilePage.newPostText = ``;
-    state.profilePage.posts.push(newPost);
-
-    renderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-}
 
 
-export default state;
+
+
+export default store;
+window.store = store;
